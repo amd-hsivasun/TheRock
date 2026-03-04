dfdca3993453a193b17da290929a6cbd0faad4fc
Danylo Lytovchenko <danylo.lytovchenko@amd.com>
2026-03-04 14:58:45 +0100

    Add opencl-cts build and run

--- build_tools/fetch_sources.py
+++ build_tools/fetch_sources.py
@@ -395,6 +395,8 @@ def get_enabled_sources(args) -> tuple[List[str], list[ExternalGitSource]]:
         projects.extend(args.media_libs_projects)
     if args.include_iree_libs:
         projects.extend(args.iree_libs_projects)
+    if args.include_opencl:
+        projects.extend(args.opencl_projects)
     if args.include_math_libraries:
         projects.extend(args.math_library_projects)
 
@@ -891,6 +893,12 @@ def main(argv):
         help="Include IREE and related libraries",
     )
     parser.add_argument(
+        "--include-opencl",
+        default=True,
+        action=argparse.BooleanOptionalAction,
+        help="Include OpenCL CTS and dependencies",
+    )
+    parser.add_argument(
         "--include-math-libraries",
         default=True,
         action=argparse.BooleanOptionalAction,
@@ -941,6 +949,18 @@ def main(argv):
         default=[
             "iree",
             "fusilli",
+        ],
+    )
+    parser.add_argument(
+        "--opencl-projects",
+        nargs="+",
+        type=str,
+        default=[
+            "opencl/opencl-cts",
+            "opencl/opencl-headers",
+            "opencl/opencl-icd-loader",
+            "opencl/spirv-headers",
+            "opencl/spirv-tools",
         ],
     )
     parser.add_argument(
